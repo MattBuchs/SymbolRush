aec617c4c147aa0238b9c646450f9c17b6fa240a
Matt Buchs <110405782+MattBuchs@users.noreply.github.com>
2025-01-09 15:49:52 +0100

    First part of the game finished

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,20 @@ import { useEffect, useState } from "react";
 export default function Home() {
     const [cards, setCards] = useState([]);
     const [symbols, setSymbols] = useState([]);
+    const [error, setError] = useState(null);
+    const [cardsFound, setCardsFound] = useState(0);
 
+    // Mélange les cartes et les symboles
+    const shuffle = (array) => {
+        const newArray = array.sort(() => Math.random() - 0.5);
+        newArray.map((card) => {
+            card.sort(() => Math.random() - 0.5);
+        });
+
+        return newArray;
+    };
+
+    // Obtiens des symboles aléatoires sans répétition
     const getRandomSymbol = (number) => {
         const availableIndices = [...symbols.keys()];
         const symbolsArr = [];
@@ -21,7 +34,17 @@ export default function Home() {
         return symbolsArr;
     };
 
+    const checkIsGoodSymbol = (e, index, indexSymbol) => {
+        const symbol = e.target.textContent;
+        const isGoodSymbol = cards[index - 1].includes(symbol);
+
+        if (isGoodSymbol) {
+            setCardsFound(index);
+        }
+    };
+
     useEffect(() => {
+        // Récupére les symboles depuis un fichier JSON
         const getSymbols = async () => {
             const apiUrl = "http://localhost:3000/emojis.json";
 
@@ -43,16 +66,15 @@ export default function Home() {
     useEffect(() => {
         if (symbols.length === 0) return;
 
-        const generateDobbleCards = (symbolsPerCard) => {
+        const generateSymbolCards = (symbolsPerCard) => {
             try {
                 const n = symbolsPerCard - 1; // n est basé sur le nombre de symboles par carte - 1
                 const totalCards = n * n + n + 1; // Nombre total de cartes
                 const randomSymbols = getRandomSymbol(totalCards);
 
-                if (randomSymbols.length < totalCards) {
-                    throw new Error(
-                        "Pas assez de symboles pour générer toutes les cartes."
-                    );
+                if (symbols.length < totalCards) {
+                    setError(true);
+                    return false;
                 }
 
                 const cards = [];
@@ -92,28 +114,60 @@ export default function Home() {
         };
 
         // Générer les cartes avec 3 symboles par carte
-        const dobbleCards = generateDobbleCards(3);
-        setCards(dobbleCards);
+        const cardsGenerated = generateSymbolCards(8);
+
+        if (!cardsGenerated) return;
+
+        const shuffleCards = shuffle(cardsGenerated);
+        setCards(shuffleCards);
     }, [symbols]);
 
     return (
-        <div>
-            <h1>Dobble Game</h1>
-            <div>
-                {cards &&
-                    cards.map((card, index) => (
-                        <div
+        <div className="bg-slate-600 min-h-screen p-4">
+            <h1 className="text-4xl text-slate-50 font-semibold text-center mt-3 mb-12">
+                SymbolRush
+            </h1>
+            {!error && cards && (
+                <ul className="flex flex-wrap justify-center gap-4">
+                    {cards.map((card, index) => (
+                        <li
                             key={index}
+                            className="w-[300px] h-[300px] flex justify-center items-center flex-wrap gap-1 p-4 rounded-lg bg-slate-50 border border-black shadow-md absolute"
                             style={{
-                                margin: "10px",
-                                padding: "10px",
-                                border: "1px solid black",
+                                top: `${index <= cardsFound ? "30%" : "75%"}`,
+                                left: `50%`,
+                                transform: `translate(-50%, -50%)`,
+                                zIndex: `${
+                                    index > cardsFound
+                                        ? cards.length - index
+                                        : index
+                                }`,
                             }}
                         >
-                            Carte {index + 1}: {card}
-                        </div>
+                            {card.map((symbol, indexSymbol) => (
+                                <button
+                                    key={`${index}-${indexSymbol}`}
+                                    onClick={(e) => {
+                                        checkIsGoodSymbol(
+                                            e,
+                                            index,
+                                            indexSymbol
+                                        );
+                                    }}
+                                    disabled={index <= cardsFound}
+                                    className={`text-[3.5rem] transition-transform${
+                                        index > cardsFound
+                                            ? " hover:scale-[0.93] hover:opacity-85"
+                                            : ""
+                                    }`}
+                                >
+                                    {symbol}
+                                </button>
+                            ))}
+                        </li>
                     ))}
-            </div>
+                </ul>
+            )}
         </div>
     );
 }
